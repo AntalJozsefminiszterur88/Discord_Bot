420c15e93d1d4d90257ce8f341e1b57fa21a002c
Valerus Santus <vatibalazs@gmail.com>
2025-12-23 20:27:12 +0100

    Update yt-dlp client options

--- bot.py
+++ bot.py
@@ -48,11 +48,7 @@ ytdl_format_options = {
     'no_warnings': True,
     'default_search': 'auto',
     'source_address': '0.0.0.0',
-    'cookiefile': 'cookies.txt',
-    'http_headers': {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-    },
-    'extractor_args': {'youtube': {'player_client': ['web']}},
+    'extractor_args': {'youtube': {'player_client': ['android', 'ios']}},
     'cachedir': False,
 }
 
